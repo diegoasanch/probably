4d47f2837102abbf0fc37da0d768d2647101aecd
Diego Sanchez <diegoasp18@gmail.com>
2021-04-17 19:19:44 -0300

    add links and version to footer

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Button, Divider, MenuItem } from '@blueprintjs/core'
+import { Button, Divider, H4, H5, H6, Icon, MenuItem } from '@blueprintjs/core'
 import { useTranslation } from 'react-i18next'
 import { Select } from '@blueprintjs/select'
 
 import { IPageInfo } from '../../types/pages'
+import styled from 'styled-components'
+import packageJSON from '../../../package.json'
 import {
     SidebarContainer,
     Header,
+    SideMain,
+    SideFooter,
 } from './styles'
-import styled from 'styled-components'
 
 type IProps = {
     readonly current_page: IPageInfo,
@@ -60,20 +63,42 @@ const Sidebar = ({
 
             <Divider />
 
-            <StyledSelect
-                // initialContent={initialContent}
-                items={available_pages}
-                itemRenderer={renderItem}
-                onItemSelect={setNewPage}
-                filterable={false}
-                noResults={<MenuItem text="No results." disabled={true} />}
-            >
-                <Button
-                    icon="function"
-                    text={translate(`select-${current_page.id}`)}
-                    rightIcon="caret-down"
-                />
-            </StyledSelect>
+            <SideMain>
+                <StyledSelect
+                    // initialContent={initialContent}
+                    items={available_pages}
+                    itemRenderer={renderItem}
+                    onItemSelect={setNewPage}
+                    filterable={false}
+                    noResults={<MenuItem text="No results." disabled={true} />}
+                >
+                    <Button
+                        icon="function"
+                        text={translate(`select-${current_page.id}`)}
+                        rightIcon="caret-down"
+                    />
+                </StyledSelect>
+                <SideFooter>
+                    <H4>
+                        <Icon className="bp3-text-muted" icon="git-branch" />&nbsp;
+                        Ver en&nbsp;
+                        <a href="https://github.com/diegoasanch/Estadistica_General">
+                            GitHub
+                        </a>
+                    </H4>
+                    <H5 className="bp3-text-muted">
+                        version: {packageJSON.version}
+                    </H5>
+                    <H6 className="bp3-text-muted">
+                        <Icon icon="code" />&nbsp;
+                        {translate('with')} 💖&nbsp;
+                        {translate('by')}&nbsp;
+                        <a href="https://github.com/diegoasanch" target="_blank" rel="noopener noreferrer">
+                            Diego.
+                        </a>
+                    </H6>
+                </SideFooter>
+            </SideMain>
         </SidebarContainer>
     )
 }
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -8,7 +8,7 @@ export const SidebarContainer = styled.nav`
     width: 100%;
     color: ${props => props.theme.sidebarText};
     background-color: ${ props => props.theme.sidebarBg };
-    border-right: 3px solid "#CACBCC"
+    border-right: 3px solid "#CACBCC";
 `
 
 export const Header = styled(H1)`
@@ -18,4 +18,20 @@ export const Header = styled(H1)`
     word-wrap: break-word;
     width: 100%;
     padding: .5em .4em;
+`
+
+export const SideMain = styled.main`
+    position: relative;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const SideFooter = styled.footer`
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 1em;
+    align-items: flex-start;
 `
