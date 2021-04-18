d03be6d7f814116b336b5e4a4abee398118934d4
Diego Sanchez <diegoasp18@gmail.com>
2021-04-17 21:42:18 -0300

    Fix warnings

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,6 @@ import i18n from 'i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
-import { homepage } from '../package.json'
 
 const { origin, pathname } = window.location
 
